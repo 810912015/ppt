18ebe442c6a4d219f76cb351cde9433b627984c8
zxf <810912015@qq.com>
2020-03-25 06:35:30 +0800

    lc done

--- lc-spider.js
+++ lc-spider.js
@@ -1,22 +1,38 @@
 const pt=require("puppeteer")
-    const url="https://leetcode-cn.com/problems/intersection-of-two-linked-lists/";
+// 导入设备描述库
+//const devices = require('puppeteer/DeviceDescriptors');
+const url="https://leetcode-cn.com/problems/groups-of-special-equivalent-strings/";
 
 (async ()=>{
     function log() {
         console.log(new Date().toLocaleString(), arguments)
     }
+    //log(devices)
     const browser=await  pt.launch({
-        headless:true,
+        headless:false,
         slowMo:250,
-        devTools:true
+        devTools:false
     })
-    const page=await browser.newPage()
+    //await browser.evaluate(devices['ipad'])
+     const page=await browser.newPage()
+    await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
+    await page.setRequestInterception(true);
+    await page.on('request',request=>{
+        let url=request.url().toString();
+        if(url.indexOf(".gif?")>-1){
+            request.abort();
+            return;
+        }
+        request.continue();
+    })
     //page.setDefaultTimeout(65000)
     await page.goto(url,{
         timeout:0
     })
     //await page.waitFor(30000);
-
+    // page.on('console',async msg=>{
+    //     log('console',msg.text())
+    // })
     async function makeOne(p) {
 
        let vr=await p.evaluate(()=>{
@@ -24,7 +40,9 @@ const pt=require("puppeteer")
                let tb = document.querySelector("#lang-select").getBoundingClientRect()
                let bb = {
                    x: tb.x,
-                   y: tb.y
+                   y: tb.y,
+                   w:tb.width,
+                   h:tb.height
                }
                const rs = {
                    page: ".pagination-screen__DnGE",
@@ -52,20 +70,30 @@ const pt=require("puppeteer")
                r.bb = bb;
                return r;
            }catch (e) {
+               console.log("basic",JSON.stringify(e),e)
                return null;
            }
        })
 
-
+       if(vr==null) return null;
 
 
         let bb=vr.bb;
         let delta=bb.y+50;
-        for(let i=0;i<6;i++){
-            delta+=30
+        for(let i=0;i<14;i++){
             await p.tap("#lang-select")
+            delta+=30
+            if(i==6){
+                let tx=bb.x+bb.w-2;
+                let ty=bb.y+bb.h+10;
+                await p.mouse.move(tx,ty);
+                await p.mouse.down();
+                await p.mouse.move(tx,ty+100);
+                await p.mouse.up();
+                delta=bb.y+100;
+            }
             await p.mouse.click(bb.x,delta)
-            await p.waitFor(1000)
+            //await p.waitFor(1000)
             let tla= await p.evaluate(()=>{
                 try {
                     const ls = {
@@ -81,9 +109,11 @@ const pt=require("puppeteer")
                     }
                     return la;
                 }catch (e) {
+                    console.log("source",JSON.stringify(e),e)
                     return null;
                 }
             })
+            //log(i,JSON.stringify(tla))
             if(tla!=null) {
                 vr.ls.push(tla)
             }
@@ -94,20 +124,27 @@ const pt=require("puppeteer")
     }
 
     let fs=require('fs');
-    for(let i=0;i<1000;i++){
+    for(let i=893;i<1581;i++){
         try {
             log("start " + i)
             let ta = await makeOne(page)
-            let fa = "result/r" + (ta.page.replace("/", "-")) + ".txt";
+            if(ta==null) {
+                log(i+" get fail")
+                await page.tap("#next-question-btn")
+                continue;
+            }
+            let fa = "result4/r" + (ta.page.replace("/", "-")) + ".txt";
             fs.appendFile(fa, JSON.stringify(ta) + "\n\n", function (error) {
                 if (error) {
                     log(i + "写入失败")
                 }
             })
-            await page.tap("#next-question-btn")
         }catch (e) {
+            log("r0",JSON.stringify(e),e)
             continue;
         }
+        await page.tap("#next-question-btn")
+        //await page.waitFor(1000)
     }
     await browser.close();
 })()
